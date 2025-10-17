52876c7701cf99044a85028e248fdee35590cdde
khbeieb <hbaiebk@science.regn.net>
2025-10-17 14:09:25 +0100

    Revert to headless

--- e2e-selenium/src/test/java/org/mobelite/base/BaseTest.java
+++ e2e-selenium/src/test/java/org/mobelite/base/BaseTest.java
@@ -34,7 +34,7 @@ public class BaseTest {
   public void setUp() throws Exception {
 
     ChromeOptions options = new ChromeOptions();
-    //options.addArguments("--headless=new");
+    options.addArguments("--headless=new");
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
     options.addArguments("--disable-gpu");
